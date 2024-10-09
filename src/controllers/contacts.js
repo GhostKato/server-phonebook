50f07b385572857597614ef184fb22f2eccbf25f
GhostKato <osadchenko.maksym@gmail.com>
2024-10-10 00:21:14 +0300

    fixed

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -69,7 +69,7 @@ export const createContactController = async (req, res) => {
 
 export const patchContactController = async (req, res, next) => {
   const { contactId } = req.params;
-  const updatedContact = await updateContact(contactId, req.body, req.user._id);
+  const updatedContact = await updateContact(contactId, req.user._id, req.body);
 
   if (!updatedContact) {
     next(createHttpError(404, 'Contact not found'));
