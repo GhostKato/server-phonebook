b27a11a9e56c9720d47a25b2058bb8de568c2930
GhostKato <osadchenko.maksym@gmail.com>
2024-12-08 13:41:13 +0200

    edit-validation

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -2,17 +2,17 @@ import Joi from 'joi';
 
 export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().min(11).max(30).required(),
+  email: Joi.string().email().min(11).max(35).required(),
   password: Joi.string().min(5).max(20).required(),
 });
 
 export const loginUserSchema = Joi.object({
-  email: Joi.string().email().min(11).max(30).required(),
+  email: Joi.string().email().min(11).max(35).required(),
   password: Joi.string().min(5).max(20).required(),
 });
 
 export const resetEmailSchema = Joi.object({
-  email: Joi.string().email().min(11).max(30).required(),
+  email: Joi.string().email().min(11).max(35).required(),
 });
 
 export const resetPasswordSchema = Joi.object({
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -5,7 +5,7 @@ export const createContactSchema = Joi.object({
   phoneNumber: Joi.string()
     .pattern(/^\+380\d{9}$/)
     .required(),
-  email: Joi.string().email().min(11).max(30),
+  email: Joi.string().email().min(11).max(35),
   isFavourite: Joi.boolean().default(false),
   contactType: Joi.string()
     .valid('work', 'home', 'personal')
@@ -16,7 +16,7 @@ export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string()
     .pattern(/^\+380\d{9}$/),
-  email: Joi.string().email().min(11).max(30),
+  email: Joi.string().email().min(11).max(35),
   isFavourite: Joi.boolean().default(false),
   contactType: Joi.string()
     .valid('work', 'home', 'personal')
