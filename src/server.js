654985904efd076ec9fd219d895f8003b8b9dfc5
GhostKato <osadchenko.maksym@gmail.com>
2024-12-10 00:00:32 +0200

    edit

--- src/server.js
+++ src/server.js
@@ -18,6 +18,8 @@ export const setupServer = () => {
   app.use(cors({
   origin: ['https://app-phonebook.vercel.app', 'http://localhost:5174', 'http://localhost:5173'],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
 }));
 
 
